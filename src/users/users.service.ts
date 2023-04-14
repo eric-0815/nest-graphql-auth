3eb1bce14689122e49c78027ddebce04b91723eb
Eric Hong <eric.hong@intelicare.com.au>
2023-04-14 14:43:31 +0800

    setup project

---
+++ src/users/users.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    {
+      id: 1,
+      username: 'eric',
+      password: '123',
+    },
+    {
+      id: 2,
+      username: '123',
+      password: '123',
+    },
+  ];
+
+  create(createUserInput: CreateUserInput) {
+    const user = {
+      ...createUserInput,
+      id: this.users.length + 1,
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(username: string) {
+    return this.users.find((user) => user.username === username);
+  }
+
+  // update(id: number, updateUserInput: UpdateUserInput) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+}
